df38841c34fcaed396f2f499f08efb9359f8dd68
Breno Henrique <brenosc2@hotmail.com>
2023-05-10 11:50:27 -0300

    initial implementation of mandelbrot set plot

---
+++ mandelbrot.cpp
@@ -0,0 +1,56 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_timer.h>
+#include <complex>
+#include <numeric>
+
+int is_in_set(std::complex<double> c) {
+  std::complex<double> z(0, 0);
+
+  for (int i = 0; i < 25; i++) {
+    z = std::pow(z, 2) + c;
+
+    if (std::norm(z) > 10) {
+      return i;
+    }
+  }
+
+  return 0;
+}
+
+int main() {
+  SDL_Init(SDL_INIT_EVERYTHING);
+
+  SDL_Window *window = nullptr;
+  SDL_Renderer *renderer = nullptr;
+
+  SDL_CreateWindowAndRenderer(1000, 1000, 0, &window, &renderer);
+  SDL_RenderSetScale(renderer, 1, 1);
+
+  for (double x = 0.0; x < 1.0; x += 0.001)
+    for (double y = 0.0; y < 1.0; y += 0.001) {
+      double point_x = std::lerp(-2.0, 2.0, x);
+      double point_y = std::lerp(-2.0, 2.0, y);
+
+      int iters = is_in_set(std::complex<double>(point_x, point_y));
+
+      if (iters == 0) {
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderDrawPointF(renderer, x * 1000, y * 1000);
+      } else {
+        SDL_SetRenderDrawColor(
+          renderer,
+          4 * iters % 255,
+          8 * iters % 255,
+          16 * iters % 255,
+          255
+        );
+        SDL_RenderDrawPointF(renderer, x * 1000, y * 1000);
+      }
+    }
+
+  SDL_RenderPresent(renderer);
+  SDL_Delay(10000);
+
+  return 0;
+}
